44c0c31365527685fef2b9e0ad7cc68aabac3a98
AMARTYA <amartyadey24@gmail.com>
2021-09-10 10:34:50 -0700

    Alert Dialog on press Exit Button Inserted!!

--- app/src/main/java/com/example/braintrainerfinal/GamePage.java
+++ app/src/main/java/com/example/braintrainerfinal/GamePage.java
@@ -1,12 +1,15 @@
 package com.example.braintrainerfinal;
 
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Trace;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -38,6 +41,10 @@ public class GamePage extends AppCompatActivity {
         numOfQuestion = 0;
         score.setText("0/0");
         countDownTimer.start();
+        option0.setEnabled(true);
+        option1.setEnabled(true);
+        option2.setEnabled(true);
+        option3.setEnabled(true);
 
     }
 
@@ -87,8 +94,17 @@ public class GamePage extends AppCompatActivity {
     }
 
     public void exit(View view){
-        Intent intentExit = new Intent(this,MainActivity.class);
-        startActivity(intentExit);
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_info)
+                .setTitle("Do you want to exit?")
+                .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finishAffinity();
+                    }
+                })
+                .setNegativeButton("NO",null)
+                .show();
     }
 
     @Override
@@ -123,6 +139,10 @@ public class GamePage extends AppCompatActivity {
                 results.setText("DONE!");
                 restart.setVisibility(View.VISIBLE);
                 restart.setText("Play Again !!");
+                option0.setEnabled(false);
+                option1.setEnabled(false);
+                option2.setEnabled(false);
+                option3.setEnabled(false);
             }
         }.start();
     }
